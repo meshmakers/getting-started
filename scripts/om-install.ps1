f1ecf9d333f50af794e2492dc2124a418d8f5be9
Jakub Poddębniak <jakub.poddebniak@meshmakers.io>
2026-04-14 10:18:36 +0200

    AB#3772: Wrong winget package identifier for OpenSSL in local architecture setup (#2)
    * AB#3893: Fix: Update OpenSSL winget identifier in documentation
    
    * AB#3893: Fix: Typo in comment
    
    * AB#3893: Fix: Typo in comment

--- scripts/om-install.ps1
+++ scripts/om-install.ps1
@@ -25,7 +25,7 @@ function Test-Prerequisites
     {
         Write-Host "NOT FOUND" -ForegroundColor Red
         Write-Host "  Please install OpenSSL and ensure it is available in PATH." -ForegroundColor Yellow
-        Write-Host "  Windows: winget install -e --id ShiningLight.OpenSSL" -ForegroundColor Yellow
+        Write-Host "  Windows: winget install -e --id ShiningLight.OpenSSL.Dev" -ForegroundColor Yellow
         Write-Host "  macOS: brew install openssl" -ForegroundColor Yellow
         $allPassed = $false
     }
@@ -464,10 +464,10 @@ ADAPTER_ADAPTERRT_ID="$adapterRtId"
 
 function Create-HttpsCert()
 {
-    # Install openseel (e. g. winget install -e --id ShiningLight.OpenSSL)
+    # Install OpenSSL (e. g. winget install -e --id ShiningLight.OpenSSL.Dev)
     if ((Get-Command openssl -ErrorAction SilentlyContinue).Length -eq 0)
     {
-        Write-Error "Ensure that OpenSSL is installed an available in PATH environment variable."
+        Write-Error "Ensure that OpenSSL is installed and available in PATH environment variable."
         return;
     }
     
