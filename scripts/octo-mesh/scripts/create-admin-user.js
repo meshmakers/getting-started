3426c781ebc59bc2c8c770282c909408f2084a18
Reimar Klammer <reimar.klammer@meshmakers.io>
2026-07-20 17:31:02 +0200

    AB#4417 New: Remove the Docker Compose deployment path

--- scripts/octo-mesh/scripts/create-admin-user.js
+++
@@ -1,8 +0,0 @@
-admin = db.getSiblingDB("admin");
-admin.createUser(
-  {
-    user: "octo-system-admin",
-     pwd: "OctoAdmin1",
-     roles: [ { role: "root", db: "admin" } ]
-  });
-admin.auth("octo-system-admin", "OctoAdmin1");
